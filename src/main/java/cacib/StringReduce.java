150e793814059d8f818cd79a8643938d8ac0f488
p0103317 <shehan.a@ncs.com.sg>
2020-05-15 21:31:10 +0800

    String reduce function

---
+++ src/main/java/cacib/StringReduce.java
@@ -0,0 +1,19 @@
+package cacib;
+
+public class StringReduce {
+
+    public static void main(String[] args) {
+        String input = "ACCAABBC";
+        String input2 = "ABCBBCBA";
+        String input3 = "BABABA";
+        System.out.println(stringReduce(input));
+        System.out.println(stringReduce(input2));
+        System.out.println(stringReduce(input3));
+    }
+
+    public static String stringReduce(String s){
+        String transformed = s.replace("AA", "").replace("CC", "")
+                .replace("BB", "");
+        return transformed.equals(s) ? transformed : stringReduce(transformed);
+    }
+}
