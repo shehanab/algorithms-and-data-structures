e01982183c0252d3638779c938eb607c7d079115
p0103317 <shehan.a@ncs.com.sg>
2020-05-15 15:46:47 +0800

    Character validator

---
+++ src/main/java/cacib/CharacterValidator.java
@@ -0,0 +1,32 @@
+package cacib;
+
+public class CharacterValidator {
+
+    public static void main(String[] args) {
+        String input1 = "Shehan";
+        String input2 = "shehan";
+        String input3 = "5hehan";
+        String input4 = "$hehan";
+        System.out.println(validateFirstChar(input1));
+        System.out.println(validateFirstChar(input2));
+        System.out.println(validateFirstChar(input3));
+        System.out.println(validateFirstChar(input4));
+    }
+
+    private static String validateFirstChar(String input) {
+        char c = input.charAt(0);
+
+        if(Character.isUpperCase(c)){
+            return "upper";
+        } else if (Character.isLowerCase(c)){
+            return "lower";
+        } else if (Character.isDigit(c)){
+            return "digit";
+        } else {
+            return "other";
+        }
+
+    }
+
+
+}
