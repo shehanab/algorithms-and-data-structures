9548041b3d78ddc6457786d3b423d7387282c546
p0103317 <shehan.a@ncs.com.sg>
2020-05-16 13:04:13 +0800

    Odd check

---
+++ src/main/java/cacib/OddCheck.java
@@ -0,0 +1,20 @@
+package cacib;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class OddCheck {
+
+    public static void main(String[] args) {
+        System.out.println(10 & 1);
+        System.out.println(10 >> 1 == 0);
+        System.out.println(10 % 2 == 0);
+        System.out.println(10 >> 1);
+        System.out.println(11 & 1);
+        System.out.println(11 >> 1 == 0);
+        System.out.println(11 % 2 == 0);
+        System.out.println(11 >> 1);
+
+
+
+    }
+}
