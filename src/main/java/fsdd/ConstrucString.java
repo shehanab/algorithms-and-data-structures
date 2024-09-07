a759c4aa0be4dc446f10ac870ec958526f3fd796
shehan.ab <shehan.ab@gmail.com>
2024-09-07 14:54:36 +0800

    Delete

--- src/main/java/fsdd/ConstrucString.java
+++
@@ -1,9 +0,0 @@
-package fsdd;
-
-public class ConstrucString {
-
-    public static boolean canConstructLetter(String text, String note) {
-        // Write your code here
-        return true;
-    }
-}
