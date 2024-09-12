0910e35f877928aa384ce995bb050f06a89a3fbe
shehan.ab <shehan.ab@gmail.com>
2024-09-12 19:11:04 +0800

    Given a string, find the length of the longest substring without repeating characters.

---
+++ src/main/java/logic/LongestDistinctCharString.java
@@ -0,0 +1,32 @@
+package logic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestDistinctCharString {
+
+    public static int lengthOfLongestSubstring(String s) {
+        // Initialize the set to keep track of the characters in the current window.
+        HashSet<Character> set = new HashSet<>();
+
+        // Initialize two pointers for the sliding window, and the result variable.
+        int left = 0, right = 0, maxLength = 0;
+
+        // Iterate through the string with the right pointer.
+        while (right < s.length()) {
+            // If the character at 'right' is not in the set, add it and update the maxLength.
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+                right++;
+            } else {
+                // If the character is already in the set, remove the character at 'left' and move left.
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+
+        return maxLength;
+    }
+
+}
---
+++ src/test/java/logic/LongestDistinctCharStringTest.java
@@ -0,0 +1,18 @@
+package logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LongestDistinctCharStringTest {
+
+    @Test
+    public void lengthOfLongestSubstring() {
+
+        assertEquals(4, LongestDistinctCharString.lengthOfLongestSubstring("shehan"));
+        assertEquals(3, LongestDistinctCharString.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(0, LongestDistinctCharString.lengthOfLongestSubstring(""));
+        assertEquals(8, LongestDistinctCharString.lengthOfLongestSubstring("abcdsfgw"));
+        assertEquals(1, LongestDistinctCharString.lengthOfLongestSubstring("aaaaaaaa"));
+    }
+}
